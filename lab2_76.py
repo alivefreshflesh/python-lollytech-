0e9dbf84db61dc652d8f9ad531eb3a5d17835cc6
alivefreshflesh <139790548+alivefreshflesh@users.noreply.github.com>
2025-12-15 20:09:43 +0300

    Update lab2_76.py

--- lab2_76.py
+++ lab2_76.py
@@ -1,11 +1,19 @@
 import requests
 import matplotlib.pyplot as plt
+import sys
 
-data = requests.get("https://raw.githubusercontent.com/dm-fedorov/python_basic/master/data/opendata.stat").text
+try:
+    data = requests.get("https://raw.githubusercontent.com/dm-fedorov/python_basic/master/data/opendata.stat").text
+    err_404 = "404: Not Found"
+    if data == err_404:
+        print(f"Ошибка при загрузке данных: {err_404}")
+        sys.exit(1)
+except requests.exceptions.RequestException as e:
+    print(f"Ошибка при загрузке данных: {e}")
+    sys.exit(1)
 
 lines = data.strip().split("\n")
 lines = lines[1::]
-
 pensions_2018 = []
 
 dates = []
@@ -18,6 +26,8 @@ for line in lines:
     region = parts[1]
     date = parts[2][:-3:]
     value = int(parts[3])
+    if len(parts) < 4: # строка может содержать недостаточно данных
+        continue 
     
     if name == "Средняя пенсия" and region == "Забайкальский край" and date[:4:] == "2018":
         pensions_2018.append(value)
@@ -35,4 +45,3 @@ plt.xticks(rotation=45) # чтоб  по оси абсцисс не было т�
 plt.grid(True)
 plt.tight_layout() # чтоб не было обрезанного окна (добавил из-за xticks)
 plt.show()
-
